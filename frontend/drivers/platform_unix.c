0288ef45ef80ec9fe607b8722f003b56dd429cc8
Jools Wills <buzz@exotica.org.uk>
2020-07-22 23:56:55 +0100

    Revert "(Linux) Set default saves/save states/system paths"
    This reverts commit e44242db93fe7026e20cd26f98c99f17a58598bf.
    
    we currently rely on the old behaviour in RetroPie.

--- frontend/drivers/platform_unix.c
+++ frontend/drivers/platform_unix.c
@@ -1888,12 +1888,6 @@ static void frontend_unix_get_env(int *argc,
          "thumbnails", sizeof(g_defaults.dirs[DEFAULT_DIR_THUMBNAILS]));
    fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_LOGS], base_path,
          "logs", sizeof(g_defaults.dirs[DEFAULT_DIR_LOGS]));
-   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_SRAM], base_path,
-         "saves", sizeof(g_defaults.dirs[DEFAULT_DIR_SRAM]));
-   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_SAVESTATE], base_path,
-         "states", sizeof(g_defaults.dirs[DEFAULT_DIR_SAVESTATE]));
-   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_SYSTEM], base_path,
-         "system", sizeof(g_defaults.dirs[DEFAULT_DIR_SYSTEM]));
 #endif
 
 #ifndef IS_SALAMANDER
