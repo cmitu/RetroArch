14c0944d36d6d92f9b6c3ff13f0f92a032285d17
cmitu <31816814+cmitu@users.noreply.github.com>
2021-12-31 06:12:09 +0000

    Add back the 'video_shader' parameter.
    The `video_shader` configuration parameter has been removed from RetroArch a while ago,
    in favour of per-game/core/folder shader overrides.
    
    However, the built-in shader override mechanism depends on the core name used, while `video_shader`
    can be used without any core (name) dependency, which makes it easier to configure with the RetroPie
    configuration editor.
    
    The patch will load the shader preset specified in the `video_shader` path if none of the built-in shader
    overrides have not been found. The parameter is not saved as part of any override/configuration file when
    saving is done from RetroArch's menu, the only mechanism to set it is by directly editing the conf file or via the configuration editor.

--- configuration.c
+++ configuration.c
@@ -1534,6 +1534,8 @@ static struct config_path_setting *populate_settings_path(
          settings->paths.directory_resampler, false, NULL, true);
    SETTING_PATH("video_shader_dir",
          settings->paths.directory_video_shader, true, NULL, true);
+   SETTING_PATH("video_shader",
+         settings->paths.path_shader, false, NULL, true);
    SETTING_PATH("video_filter_dir",
          settings->paths.directory_video_filter, true, NULL, true);
    SETTING_PATH("core_assets_directory",
@@ -2777,6 +2779,7 @@ void config_set_defaults(void *data)
    *settings->paths.directory_menu_config = '\0';
 #endif
    *settings->paths.directory_video_shader = '\0';
+   *settings->paths.path_shader = '\0';
    *settings->paths.directory_video_filter = '\0';
    *settings->paths.directory_audio_filter = '\0';
 
@@ -4722,6 +4725,10 @@ bool config_save_file(const char *path)
    {
       for (i = 0; i < (unsigned)path_settings_size; i++)
       {
+         /* blacklist 'video_shader', better handled by shader presets */
+         if (string_is_equal(path_settings[i].ident, "video_shader"))
+             continue;
+
          const char *value = path_settings[i].ptr;
 
          if (path_settings[i].def_enable && string_is_empty(path_settings[i].ptr))
@@ -5074,6 +5081,10 @@ bool config_save_overrides(enum override_type type, void *data)
 
       for (i = 0; i < (unsigned)path_settings_size; i++)
       {
+         /* blacklist 'video_shader', better handled by shader presets */
+         if (string_is_equal(path_settings[i].ident, "video_shader"))
+             continue;
+
          if (!string_is_equal(path_settings[i].ptr, path_overrides[i].ptr))
             config_set_path(conf, path_overrides[i].ident,
                   path_overrides[i].ptr);
